17a1bfc73fd61f3908db7f92071cc1ccaadb776b
HongDaeun <angie0412@naver.com>
2023-10-12 23:09:44 +0900

    주석 제거

--- JungleWeek00/app.py
+++ JungleWeek00/app.py
@@ -151,12 +151,5 @@ def join(locations_id):
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# @app.route('/display_users')
-# def display_users():
-#     users_info = users.find({}, {'_id': 0})
-#     users_list = [user for user in users_info]
-#     return jsonify(users_list)
-
 if __name__ == '__main__':  
    app.run('0.0.0.0', port=5000, debug=True)
